f219055f5f7d02f01a66c10f00339f2c169baff5
Kio Svensson <kiolee@mac.com>
2026-09-10 09:27:13 +0200

    Hello World App

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Kio Svensson");
+    }
+}
